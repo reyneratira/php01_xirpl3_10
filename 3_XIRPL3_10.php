5ef653dbcd90de3ce14b8f1e3f11b0b3cecc3e1c
dhodo999 <91859232+dhodo999@users.noreply.github.com>
2021-11-11 19:08:59 +0700

    Add files via upload

---
+++ 3_XIRPL3_10.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<head>
+    <title>Array</title>
+</head>
+
+<?php
+$nama = array(
+    0=>"Satria",
+    1=>"Arsyi",
+    2=>"Dhika",
+    3=>"Evan",
+    4=>"Fabian",
+    5=>"Irfan",
+);
+    echo "<pre>";
+    print_r($nama);
+?>
