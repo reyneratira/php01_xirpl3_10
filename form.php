99eb312e80ef27f52932dc8753173095400ab5a5
Nicolaus "Star" Natanael <81279041+Crimson-Squadron@users.noreply.github.com>
2021-11-12 23:22:28 +0700

    Add files via upload

---
+++ form.php
@@ -0,0 +1,7 @@
+<form action="proses.php" method="get">
+    Nama: <input type="text" name="nama" />
+    <br />
+    E-Mail: <input type="text" name="email" />
+    <br />
+    <input type="submit" value="Proses Data">
+</form>
